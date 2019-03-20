34e86e44986554354a3b9900e253bbededb72044
Qebuqci <ricardokae.rj@gmail.com>
2019-03-20 12:02:06 +0000

    Created '/answer' endpoint

--- server/routes/health.py
+++ server/routes/health.py
@@ -7,7 +7,7 @@ def health():
     """health route"""
     state = {"status": "UP"}
     return jsonify(state)
-    
+
 @app.route('/answer')
 def answer():
     answer = {'The Answer to Life the Universe and Everything': 42}
